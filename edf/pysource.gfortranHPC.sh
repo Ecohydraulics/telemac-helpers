91dbf30ee3c95c8b9d499bcfa8fba2962dc1af90
sschwindt <sebastian.schwindt@alumni.epfl.ch>
2024-04-24 16:10:35 +0200

    updated to v8p5 and removed MED and HDF5 installation dependencies

--- edf/pysource.gfortranHPC.sh
+++ edf/pysource.gfortranHPC.sh
@@ -6,7 +6,7 @@
 ### TELEMAC settings -----------------------------------------------------------
 ###
 # Path to telemac root dir
-export HOMETEL=/home/USER-NAME/telemac/v8p4r0
+export HOMETEL=/home/USER-NAME/telemac/v8p5r0
 # Adding python scripts to PATH
 export PATH=$HOMETEL/scripts/python3:.:$PATH
 # Configuration file
@@ -39,18 +39,20 @@ export LD_LIBRARY_PATH=$PATH/lib:$LD_LIBRARY_PATH
 ###
 ### EXTERNAL LIBRARIES -----------------------------------------------------------
 ###
+### Note: better use Q4TS QGIS plugin for MED file handling
 ### HDF5 -----------------------------------------------------------
-export HDF5HOME=$SYSTEL/hdf5
-export LD_LIBRARY_PATH=$HDF5HOME/lib:$LD_LIBRARY_PATH
-export LD_RUN_PATH=$HDF5HOME/lib:$MEDHOME/lib:$LD_RUN_PATH
+# export HDF5HOME=$SYSTEL/hdf5
+# export LD_LIBRARY_PATH=$HDF5HOME/lib:$LD_LIBRARY_PATH
+# export LD_RUN_PATH=$HDF5HOME/lib:$MEDHOME/lib:$LD_RUN_PATH
 ### MED  -----------------------------------------------------------
-export MEDHOME=$SYSTEL/med-4.0.0
-export LD_LIBRARY_PATH=$MEDHOME/lib:$LD_LIBRARY_PATH
-export PATH=$MEDHOME/bin:$PATH
+# export MEDHOME=$SYSTEL/med-4.0.0
+# export LD_LIBRARY_PATH=$MEDHOME/lib:$LD_LIBRARY_PATH
+# export PATH=$MEDHOME/bin:$PATH
 ### MUMPS -------------------------------------------------------------
 #export MUMPSHOME=$SYSTEL/LIBRARY/mumps/gnu
 #export SCALAPACKHOME=$SYSTEL/LIBRARY/scalapack/gnu
 #export BLACSHOME=$SYSTEL/LIBRARY/blacs/gnu
+###
 ### METIS -------------------------------------------------------------
 export METISHOME=$SYSTEL/metis/build/
 export LD_LIBRARY_PATH=$METISHOME/lib:$LD_LIBRARY_PATH
